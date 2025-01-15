89113e0db4a6841a26531b1412ae2170442b272b
Lance Onnagan <lronnagan@student.apc.edu.ph>
2025-01-15 14:53:36 +0800

    vue

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,29 +1,12 @@
-const app3 = Vue.createApp({
-    data() {
-      return {
-        itemName: null,
-        itemNumber: null,
-        itemImportant: false,
-        shoppingList: [
-          { name: 'n/a', number: 5, important: false, found: false },
-          { name: 'b/a', number: 1, important: false, found: false },
-          { name: 'n/a', number: 1, important: true, found: true }
-        ]
-      }
-    },
-    methods: {
-      addItem(){
-        let item = {
-          name: this.itemName,
-          number: this.itemNumber,
-          important: this.itemImportant,
-          found: false
-        }
-        this.shoppingList.push(item)
-        this.itemName = null
-        this.itemNumber = null  
-        this.itemImportant = false
-      }
-    }
-  })
- app3.mount('#app3')
+const app = Vue.createApp({
+  data() {
+   return {
+     images: [
+       {name: 'Picture 1', url: 'home/md.png'},
+       {name: 'Picture 2', url: 'home/pfp.png'},
+       {name: 'Picture 3', url: 'home/sg.png'}
+     ]
+   }
+  }
+ })
+ app.mount('#app')
