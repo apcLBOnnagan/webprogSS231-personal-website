3f2f47a5b6bc86012a2ce0e3dfcb497d73b89127
Lance Onnagan <lronnagan@student.apc.edu.ph>
2025-01-15 15:02:12 +0800

    vue

--- home/js/vue.js
+++ home/js/vue.js
@@ -2,9 +2,9 @@ const app = Vue.createApp({
   data() {
    return {
      images: [
-       {name: 'Picture 1', url: 'home/md.png'},
-       {name: 'Picture 2', url: 'home/pfp.png'},
-       {name: 'Picture 3', url: 'home/sg.png'}
+       {name: 'Picture 1', url: 'home/md (1).png'},
+       {name: 'Picture 2', url: 'home/pfp (1).png'},
+       {name: 'Picture 3', url: 'home/sg (1).png'}
      ]
    }
   }
